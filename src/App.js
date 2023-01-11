4cb04c1cb0413cfb02dee97d5ebe4de67b880158
Veny Cordova <venycordova@gmail.com>
2023-01-11 16:48:20 -0500

    adding bootstrap elemnts to the webpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import TopBar from './Components/TopBar'
+import StoreListings from './Components/StoreListings'
+import Background from './Components/Background.js'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar />
+      <Background />
+      <StoreListings />
     </div>
   );
 }
