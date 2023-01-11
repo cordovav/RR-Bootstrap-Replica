8c598ffe190e27f78bf6c03d494b4e0dcb0374df
Veny Cordova <venycordova@gmail.com>
2023-01-11 16:52:16 -0500

    adding bootstrap elemnts to the webpage pt2

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -15,7 +15,7 @@ export default function TopBar() {
 
                     <Nav className="mr-auto"></Nav>
                     <Nav style={{ padding: "24px 40px 20px 40px", fontSize: "18px", lineHeight: "26px", fontWeight: "bolder", display: "flex"}}>
-                        <Nav.Link style={{ color: "#343538"}} href="#LogIn">Log In</Nav.Link>
+                        <Nav.Link style={{ color: "#343538" }} href="#LogIn">Log In</Nav.Link>
                         <Button variant="success" style={{ fontSize: "18px" }}>Sign Up</Button> {' '}
                     </Nav>
                 </Navbar.Collapse>
